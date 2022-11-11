e91a2b84588564a0c7c582ea9d452e5f3ecb6a02
marocena26 <mariaelena.arocena@gmail.com>
2022-11-11 23:48:40 +0100

    paint charactersList in the HTML, created render function

--- src/js/0_renderBreakinBad.js
+++ src/js/0_renderBreakinBad.js
@@ -1,18 +1,48 @@
 'use strict';
 
-// QUERYSELECTOR
+//--------------------CONSTANTES--------------------//
 
 const charactersList = document.querySelector('.js__characters');
 const favouritesList = document.querySelector('.js__favourites');
 
-//VARIABLES GLOBALES -> VARIABLES CON DATOS DE LA APLICACIÓN (PERSONAJES Y FAVORITOS)
+//--------------------VARIABLES--------------------//
 
-//FUNCIONES
+let myCharactersList = []; //Array vacío. Necesitamos una variable (let) que guarde los datos que vamos a traernos de la API de BB.
 
-//EVENTOS
+let myFavouritesList = []; //Array vacío. Necesitamos una variable (let) que guarde los datos que vamos a traernos de la API de BB marcados como favoritos.
 
-//PETICIONES AL SERVIDOR
+//--------------------FUNCIONES--------------------//
+
+function renderCharacter(character) {
+  let html = `
+    <li><img src="${character.img}" alt=""></li>
+    <li>${character.name}</li>
+    <li>${character.status}</li>`;
+  return html;
+}
+
+function renderCharactersList () {
+  let html = '';
+
+  for (let i = 0; i < myCharactersList.length; i++) {
+    html += renderCharacter(myCharactersList[i]);
+  }
+
+  charactersList.innerHTML = html;
+
+  //addShipsListerers();
+}
+
+//---------------------EVENTOS---------------------//
+
+//----CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA----//
+
+renderCharactersList();
 
 fetch('https://breakingbadapi.com/api/characters')
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((jsonData) => {
+    myCharactersList = jsonData;
+
+    renderCharactersList();
+  });
