8d3ca9c00d315d281084652d55668356bc44b554
marocena26 <mariaelena.arocena@gmail.com>
2022-11-11 21:24:43 +0100

    start create structure JS: querySelector

--- src/js/0_renderBreakinBad.js
+++ src/js/0_renderBreakinBad.js
@@ -2,10 +2,17 @@
 
 // QUERYSELECTOR
 
+const charactersList = document.querySelector('.js__characters');
+const favouritesList = document.querySelector('.js__favourites');
+
 //VARIABLES GLOBALES -> VARIABLES CON DATOS DE LA APLICACIÓN (PERSONAJES Y FAVORITOS)
 
 //FUNCIONES
 
 //EVENTOS
 
-//CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA
+//PETICIONES AL SERVIDOR
+
+fetch('https://breakingbadapi.com/api/characters')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
