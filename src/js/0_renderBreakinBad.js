c6515a9a899339e542ab594f6c03e282b0b09756
marocena26 <mariaelena.arocena@gmail.com>
2022-11-12 11:39:17 +0100

    add id atribute  to all objects characters for search

--- src/js/0_renderBreakinBad.js
+++ src/js/0_renderBreakinBad.js
@@ -4,12 +4,14 @@
 
 const charactersList = document.querySelector('.js__characters');
 const favouritesList = document.querySelector('.js__favourites');
+const searchInput = document.querySelector('.js__search_input');
+const searchBtn = document.querySelector('.js__search_btn');
 
 //--------------------VARIABLES--------------------//
 
 let myCharactersList = []; //Array vacío. Necesitamos una variable (let) que guarde los datos que vamos a traernos de la API de BB.
 
-let myFavouritesList = []; //Array vacío. Necesitamos una variable (let) que guarde los datos que vamos a traernos de la API de BB marcados como favoritos.
+let myFavouritesList = []; //Array vacío. Necesitamos una variable (let) que guarde solo los datos en los que hayamos hecho click y que estén marcados como favoritos.
 
 //--------------------FUNCIONES--------------------//
 
@@ -20,7 +22,7 @@ function renderCharacter(character) { //el parámetro 'character' obtiene la inf
   let liElement = //Variable vacía dónde voy a ir añadiendo todo lo que me devuelve la función.
 
     `<li>
-    <article>
+    <article class="js__characters_article" id="${character.char_id}"> 
         <img src="${character.img}" alt="" class="characters__ul--img">
         <h3 class="characters__ul--name">${character.name}</h3>
         <p class="characters__ul--status">${character.status}</p>
@@ -39,11 +41,32 @@ function renderCharactersList () {
   }
 
   charactersList.innerHTML = liElement; //Todo lo acumulado en la variable se pinta en mi array vacío de personajes.
+  selectFavouriteCharacters(); //Asigna el evento después de creear todos los elementos. 
 
-  //addShipsListerers();
 }
 
+//Función para asignar un evento a todos los elementos de mi página.
+function selectFavouriteCharacters() {
+  const charactersArticle = document.querySelectorAll('.js__characters_article'); //Utilizamos querySelectorAll para coger todos los <article> de mi <ul>. Generamos un array de elementos.
+
+  for (const eachCharactersArticle of charactersArticle) { //Tenemos que hacer un bucle for para poder ponerle el evento a cada artículo, ya que no es posible ponerselo directamente a un array.
+    eachCharactersArticle.addEventListener('click', handleClickFavourites); //Escuchará el evento cuando haga click sobre un personaje.
+  }
+}
+
+
 //---------------------EVENTOS---------------------//
+
+function handleClickFavourites(event) {
+  console.log('¡Has hecho cliiiick');
+  event.currentTarget.classList.toggle('selected');
+  console.log( event.currentTarget.id);
+  myCharactersList.find((eachCharacter) => eachCharacter.id === event.currentTarget.id); //find va a ir a cada objeto del array, me devolverá el primero que encuentre con ese id (el que tenga el objeto que he clicado)
+
+ 
+
+
+}
 
 //----CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA----//
 
