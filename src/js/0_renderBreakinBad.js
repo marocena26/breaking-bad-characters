e7d0fbaafd3f492270b9ed4c09f38c5788583363
marocena26 <mariaelena.arocena@gmail.com>
2022-11-13 14:21:50 +0100

    add function favourites

--- src/js/0_renderBreakinBad.js
+++ src/js/0_renderBreakinBad.js
@@ -74,10 +74,12 @@ function handleClickFavourites(event) {
   console.log('Prueba');
   event.currentTarget.classList.toggle('selected');
   console.log(event.currentTarget.id);
+  
   const selectFavouriteObj = myCharactersList.find((eachCharacter) =>  eachCharacter.char_id === parseInt(event.currentTarget.id)); //find va a ir a cada objeto del array, me devolverá el primero que encuentre con ese id (el que tenga el objeto que he clicado).
   console.log(selectFavouriteObj); //NO FUNCIONA.
 
   myFavouritesList.push(selectFavouriteObj); //añado al array vacío de favoritos el objeto que he seleccionado con el click.
+  renderFavouritesList();
 }
 
 //----CÓDIGO QUE SE EJECUTA AL CARGAR LA PÁGINA----//
